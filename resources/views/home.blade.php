8f58108c83c9c98a158db7f2472f0d59191d12af
DenisConsta <denisconstantin338@gmail.com>
2023-01-09 15:53:03 +0100

    [init] first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container">
+        <h1 class="display-2 text-center py-3">Benvenuto</h1>
+
+        <div class="row row-cols-1 row-cols-lg-2">
+            <div class="col">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit vel, aut, unde excepturi perferendis accusamus accusantium pariatur qui assumenda nostrum placeat, reiciendis sapiente sed! Similique tenetur minus quas esse explicabo non beatae molestias vitae? Vitae vero accusamus obcaecati excepturi repellendus voluptatum possimus asperiores ipsa doloremque rem illum distinctio, dicta quas facere ullam! Unde, quo doloribus tempora harum soluta assumenda doloremque aut debitis, eius corrupti praesentium odio atque aliquid necessitatibus voluptatem eveniet! Expedita in libero dignissimos provident, veniam voluptatem incidunt assumenda, deserunt illum ut quam, fugiat laudantium amet cumque quod commodi nostrum voluptas accusamus ipsum reprehenderit placeat possimus temporibus! Excepturi suscipit molestias, tempore nostrum quod quaerat magnam facilis. Debitis optio, quae reprehenderit ut in placeat non minus at nobis dignissimos modi ipsam, consequatur unde enim, obcaecati tempora animi veniam recusandae laborum. Sit facilis quam, quidem, odit nihil magni odio voluptates voluptas ullam eum maxime molestias iure, quae laudantium! Beatae sed aspernatur nihil possimus, vero nisi, ut dolorem aperiam velit sapiente quas quibusdam ex itaque ullam omnis eaque eligendi blanditiis. Culpa ad iste asperiores recusandae inventore? Officiis nulla aliquid aspernatur ab dolorum incidunt repellat! Maxime cum voluptate accusamus mollitia excepturi recusandae nisi blanditiis ullam, hic laboriosam autem iste, deserunt, porro rerum. Explicabo!
+                </p>
+            </div>
+            <div class="col">
+                <img class="img-fluid" src=" {{ Vite::asset('resources/img/classic-movies.jpg') }} " alt="">
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Movies</a>
+            <button class="navbar-toggler" type="button">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link {{Route::currentRouteName() === 'home' ? 'active' : ''}}" href="{{route('home')}}">Home</a>
+                    <a class="nav-link {{Route::currentRouteName() === 'movies' ? 'active' : ''}}" href="{{route('movies')}}">Movies</a>
+                    <a class="nav-link {{Route::currentRouteName() === 'about' ? 'active' : ''}}" href="#">About Us</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
